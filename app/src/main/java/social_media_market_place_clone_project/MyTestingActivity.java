b43674bad4054392ad15e21065441f4f7adbe68c
Mervynn Shoko <mervynnshoko@gmail.com>
2021-04-07 11:16:04 +0200

    testing

---
+++ app/src/main/java/social_media_market_place_clone_project/MyTestingActivity.java
@@ -0,0 +1,15 @@
+package social_media_market_place_clone_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.activity.R;
+
+public class MyTestingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_testing);
+    }
+}
