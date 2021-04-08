7ddfdfd5d7d6af8a29d99cebe0cb7ceff5c333f2
Datumza <reitumetsemasilo@gmail.com>
2021-04-08 11:25:16 +0200

    Reitumetse Android Studio

---
+++ app/src/main/java/com/example/social_media_market_place_clone_project/SignIn.java
@@ -0,0 +1,14 @@
+package com.example.social_media_market_place_clone_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
