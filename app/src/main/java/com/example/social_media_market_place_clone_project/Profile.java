310ce5aa3df4a5848822f62da6a827257f3c4cf6
Merv101 <69701044+Merv101@users.noreply.github.com>
2021-04-10 17:52:14 +0200

    mervynn signin

--- app/src/main/java/com/example/social_media_market_place_clone_project/Profile.java
+++ app/src/main/java/com/example/social_media_market_place_clone_project/Profile.java
@@ -3,6 +3,9 @@ package com.example.social_media_market_place_clone_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.HashMap;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -13,11 +16,17 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        SessionManager session = new SessionManager(Profile.this);
+        session.checkLogin();
+        HashMap<String,String> currentUser = session.getUserDetails();
+
+        Toast.makeText(Profile.this,currentUser.get(session.EMAIL),Toast.LENGTH_SHORT).show();
         details = findViewById(R.id.displayCurrentUser);
 
         SessionManager sessionManager = new SessionManager(Profile.this);
         sessionManager.checkLogin();
-        HashMap<String, String> currentUser = sessionManager.getUserDetails();
+        //HashMap<String, String> currentUser = sessionManager.getUserDetails();
 
 
         String email = currentUser.get(sessionManager.EMAIL);
