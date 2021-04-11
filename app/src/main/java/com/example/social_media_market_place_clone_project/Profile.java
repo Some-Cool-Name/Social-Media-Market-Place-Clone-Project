06e16efe397f0d5b5c1c3092bab4f498e2fbc370
Muda-u <vhugala.m00@gmail.com>
2021-04-11 10:15:25 +0200

    with logout

--- app/src/main/java/com/example/social_media_market_place_clone_project/Profile.java
+++ app/src/main/java/com/example/social_media_market_place_clone_project/Profile.java
@@ -3,18 +3,34 @@ package com.example.social_media_market_place_clone_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class Profile extends AppCompatActivity {
-
-    // Disable back button
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
-
+    TextView details;
+    Button logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        details = findViewById(R.id.displayCurrentUser);
+        logout= findViewById(R.id.logout_btn);
+        SessionManager sessionManager = new SessionManager(Profile.this);
+        sessionManager.checkLogin();
+        HashMap<String, String> currentUser = sessionManager.getUserDetails();
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sessionManager.logoutUser();
+            }
+        });
+        Toast.makeText(Profile.this,currentUser.get(sessionManager.EMAIL),Toast.LENGTH_SHORT).show();
+
+        String email = currentUser.get(sessionManager.EMAIL);
+        details.setText(email);
     }
 }
