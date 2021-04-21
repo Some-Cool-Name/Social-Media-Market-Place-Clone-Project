7415d4e1aeb3563340df51a5b01cc69b8a66d4eb
Datumza <reitumetsemasilo@gmail.com>
2021-04-21 19:03:33 +0200

    Reitumetse Android Studio (Sprint 2)

--- app/src/main/java/com/example/social_media_market_place_clone_project/Profile.java
+++ app/src/main/java/com/example/social_media_market_place_clone_project/Profile.java
@@ -3,11 +3,14 @@ package com.example.social_media_market_place_clone_project;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +21,7 @@ import java.time.LocalDate;
 import java.time.Period;
 
 
-public class Profile extends AppCompatActivity {
+public class Profile extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     TextView name,age,location,bio;
     Button logout;
     ImageView imageView;
@@ -34,23 +37,39 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewprofile);
+
         SessionManager sessionManager = new SessionManager(Profile.this);
         sessionManager.checkLogin();
         HashMap<String, String> currentUser = sessionManager.getUserDetails();
 
-        name=findViewById(R.id.txtName);
-        name.setText(currentUser.get("FULLNAME"));
-        age=findViewById(R.id.txtAge);
+        name=findViewById(R.id.txtNameAge);
+
         AgeCalculator ageCalculator = new AgeCalculator();
-        age.setText(ageCalculator.calculateAge(currentUser.get("BIRTHDAY")).toString()); // create function to automatically calculate age
+
+        //age=findViewById(R.id.txtAge);
+        //name.setText(currentUser.get("FULLNAME"));
+        //age.setText(ageCalculator.calculateAge(currentUser.get("BIRTHDAY")).toString()); // create function to automatically calculate age
+
+        String n = currentUser.get("FULLNAME");
+        String a = ageCalculator.calculateAge(currentUser.get("BIRTHDAY")).toString();
+
+        name.setText(n + ", " + a);
+
+
         location=findViewById(R.id.txtLocation);
         String url = currentUser.get("PROFILE_PICTURE");
-        location.setText("Braam");
+        location.setText("Location");
         bio=findViewById(R.id.EdittxtBio) ;
         bio.setText(currentUser.get("BIO"));
 
         imageView = findViewById(R.id.profile_image);
 
+        Toast.makeText(Profile.this,"Welcome",Toast.LENGTH_SHORT).show();
+        loadImageFromUrl(url);
+        System.out.print(url);
+
+
+        /*
         logout=findViewById(R.id.logout_button);
         logout.setText("Logout");
         logout.setOnClickListener(new View.OnClickListener() {
@@ -59,15 +78,39 @@ public class Profile extends AppCompatActivity {
                 sessionManager.logoutUser();
             }
         });
-        Toast.makeText(Profile.this,"Welcome",Toast.LENGTH_SHORT).show();
-        loadImageFromUrl(url);
-        System.out.print(url);
 
-       /* SessionManager session = new SessionManager(Profile.this);
+        SessionManager session = new SessionManager(Profile.this);
         session.checkLogin();
         HashMap<String,String> currentUser = session.getUserDetails();
-        Toast.makeText(Profile.this,currentUser.get(session.EMAIL),Toast.LENGTH_SHORT).show();*/
+        Toast.makeText(Profile.this,currentUser.get(session.EMAIL),Toast.LENGTH_SHORT).show();
+        */
     }
+
+    // Drop down menu
+    public void showMenu(View v){
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.popup_menu);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.settings_item:
+                openMenu();
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    public void openMenu(){
+        Intent intent = new Intent(Profile.this, Settings.class);
+        startActivity(intent);
+    }
+    // **************************************************************
 
     private void loadImageFromUrl(String url) {
         Picasso.with(this).load(url).into(imageView, new com.squareup.picasso.Callback() {
