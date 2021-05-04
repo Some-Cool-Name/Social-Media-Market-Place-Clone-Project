748e7f3e7f854233594f88cc71c26d36275d69b0
ThandoAmatullah <1870175@students.wits.ac.za>
2021-05-04 17:09:58 +0200

    Added more test cases

--- app/src/androidTest/java/com/example/social_media_market_place_clone_project/ValidationTester.java
+++ app/src/androidTest/java/com/example/social_media_market_place_clone_project/ValidationTester.java
@@ -6,21 +6,89 @@ import static org.junit.Assert.assertEquals;
 
 public class ValidationTester {
     @Test
-    public void usernameContainsNumber(){
+    public void usernameContainsZero(){
       DataValidation dataValidation = new DataValidation();
-      String result = dataValidation.validateSignuUp2("1one");
+      String result = dataValidation.validateSignuUp2("0one");
       assertEquals("Username Cannot Contain Numbers", result);
     }
+    @Test
+    public void usernameContainsOne(){
+        DataValidation dataValidation = new DataValidation();
+        String result = dataValidation.validateSignuUp2("1one");
+        assertEquals("Username Cannot Contain Numbers", result);
+    }
+    @Test
+    public void usernameContainsTwo(){
+        DataValidation dataValidation = new DataValidation();
+        String result = dataValidation.validateSignuUp2("2one");
+        assertEquals("Username Cannot Contain Numbers", result);
+    }
+    @Test
+    public void usernameContainsThree(){
+        DataValidation dataValidation = new DataValidation();
+        String result = dataValidation.validateSignuUp2("3one");
+        assertEquals("Username Cannot Contain Numbers", result);
+    }
+    @Test
+    public void usernameContainsFour(){
+        DataValidation dataValidation = new DataValidation();
+        String result = dataValidation.validateSignuUp2("4one");
+        assertEquals("Username Cannot Contain Numbers", result);
+    }
+    @Test
+    public void usernameContainsFive(){
+        DataValidation dataValidation = new DataValidation();
+        String result = dataValidation.validateSignuUp2("5one");
+        assertEquals("Username Cannot Contain Numbers", result);
+    }
+    @Test
+    public void usernameContainsSix(){
+        DataValidation dataValidation = new DataValidation();
+        String result = dataValidation.validateSignuUp2("6one");
+        assertEquals("Username Cannot Contain Numbers", result);
+    }
+    @Test
+    public void usernameContainsSeven(){
+        DataValidation dataValidation = new DataValidation();
+        String result = dataValidation.validateSignuUp2("7one");
+        assertEquals("Username Cannot Contain Numbers", result);
+    }
+    @Test
+    public void usernameContainsEight(){
+        DataValidation dataValidation = new DataValidation();
+        String result = dataValidation.validateSignuUp2("8one");
+        assertEquals("Username Cannot Contain Numbers", result);
+    }
+    @Test
+    public void usernameContainsNine(){
+        DataValidation dataValidation = new DataValidation();
+        String result = dataValidation.validateSignuUp2("9one");
+        assertEquals("Username Cannot Contain Numbers", result);
+    }
+
     @Test
     public void usernameNotContainsNumber(){
         DataValidation dataValidation = new DataValidation();
         String result = dataValidation.validateSignuUp2("one");
         assertEquals("Valid", result);
     }
+
     @Test
-    public void emptyFields(){
+    public void emptyPassword(){
         DataValidation dataValidation = new DataValidation();
         String result = dataValidation.validateSignUp1("one@gmail.com", "", "1234");
+        assertEquals("Fill Out All Fields", result);
+    }
+    @Test
+    public void emptyEmail(){
+        DataValidation dataValidation = new DataValidation();
+        String result = dataValidation.validateSignUp1("", "1234", "1234");
+        assertEquals("Fill Out All Fields", result);
+    }
+    @Test
+    public void emptyConfirmPassword(){
+        DataValidation dataValidation = new DataValidation();
+        String result = dataValidation.validateSignUp1("one@gmail.com", "1234", "");
         assertEquals("Fill Out All Fields", result);
     }
     @Test
---
+++ app/src/test/java/com/example/social_media_market_place_clone_project/marks.py
@@ -0,0 +1,6 @@
+inputs = input("Please enter your name and grade: ");
+counter = 0;
+
+while counter < inputs:
+	counter = counter + 1
+	print(counter)
