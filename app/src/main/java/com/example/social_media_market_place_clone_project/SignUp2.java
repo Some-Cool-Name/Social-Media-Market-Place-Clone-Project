da4a408388d403cd811c7aa813fded9011e8584a
Muda-u <vhugala.m00@gmail.com>
2021-05-07 14:26:28 +0200

    check if null

--- app/src/main/java/com/example/social_media_market_place_clone_project/SignUp2.java
+++ app/src/main/java/com/example/social_media_market_place_clone_project/SignUp2.java
@@ -83,7 +83,7 @@ public class SignUp2 extends AppCompatActivity {
                 DataValidation validate = new DataValidation();
                 AgeCalculator ageCalculator = new AgeCalculator();
                 String isValid =validate.validateSignuUp2(name.getText().toString());
-               if(isValid.equals("Valid") && Integer.parseInt(ageCalculator.calculateAge(dateURLformat))>=18){
+               if(isValid.equals("Valid") &&dateURLformat!= null&& Integer.parseInt(ageCalculator.calculateAge(dateURLformat))>=18){
                     SessionManager sessionManager = new SessionManager(SignUp2.this);
                     SignUp signUp = new SignUp();
                     AsyncNetwork request  = new AsyncNetwork();
