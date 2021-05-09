f163bf4e01e71c95d793799115f9ad5cc0bcec7a
Muda-u <vhugala.m00@gmail.com>
2021-05-09 23:33:29 +0200

    checkout

--- app/src/main/java/com/example/social_media_market_place_clone_project/SignIn.java
+++ app/src/main/java/com/example/social_media_market_place_clone_project/SignIn.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -20,6 +21,7 @@ import okhttp3.HttpUrl;
 public class SignIn extends AppCompatActivity {
     EditText email, password;
     Button signIn;
+    TextView error;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +32,7 @@ public class SignIn extends AppCompatActivity {
         signIn = (Button) findViewById(R.id.sign_in_button);
         email = findViewById(R.id.editTextSignInEmailAddress);
         password = findViewById(R.id.editTextSignInPassword);
-
+        error = findViewById(R.id.textError);
         signIn.setOnClickListener(v -> {
             try {
                 doSignIn();
@@ -52,23 +54,42 @@ public class SignIn extends AppCompatActivity {
         request.execute(url);
 
         while(request.Result.equals("Waiting")){
-            Toast.makeText(SignIn.this,"Loading",Toast.LENGTH_SHORT).show();
+            //Toast.makeText(SignIn.this,"Loading",Toast.LENGTH_SHORT).show();
+            System.out.print("waiting");
+            //password.setError("Wrong things");
+
         }
 
 
         // Request is finished
         JSONObject wholeString = new JSONObject(request.Result); // Read the whole string
         JSONArray jsonArray = new JSONArray(wholeString.getJSONArray("login").toString()); // extract the login credentials array
-
-        JSONObject userCredentials = jsonArray.getJSONObject(0);
+        JSONObject userCredentials = null;
+        try{
+            userCredentials = jsonArray.getJSONObject(0);
+        }
+        catch(Exception xception ){
+            xception.printStackTrace();
+        }
+        Toast.makeText(getBaseContext(),wholeString.getString("success"),Toast.LENGTH_SHORT).show();
 
         if(wholeString.getString("success").equals("0")){
-            Toast.makeText(SignIn.this,wholeString.getString("message"),Toast.LENGTH_SHORT).show();
-        }else{
-            session.createSession(userCredentials.getString("username"),userCredentials.getString("name"),userCredentials.getString("Birthday"),userCredentials.getString("gender"),userCredentials.getString("Sexuality"), userCredentials.getString("bio"), userCredentials.getString("profile_picture"));
+            System.out.print("");
+            error.setText("wrong cred");
+
+            //return;
+
+        }else {
+            Toast.makeText(SignIn.this, "correct", Toast.LENGTH_SHORT).show();
+            session.createSession(userCredentials.getString("username"), userCredentials.getString("name"), userCredentials.getString("Birthday"), userCredentials.getString("gender"), userCredentials.getString("Sexuality"), userCredentials.getString("bio"), userCredentials.getString("profile_picture"));
             Intent intentSignIn = new Intent(SignIn.this, HomeView.class);
-            intentSignIn.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intentSignIn.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intentSignIn);
         }
+           // Toast.makeText(getBaseContext(),"Wrong login details",Toast.LENGTH_SHORT).show();
+           // System.out.print("Wrong");
+            //error.setText("H");
+
+
     }
 }
