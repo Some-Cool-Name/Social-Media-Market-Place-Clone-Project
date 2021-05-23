86f2f6e137d2f166385c14fc017dd9089cccd2d9
Vuyo <hlumi68@gmail.com>
2021-05-23 15:48:21 +0200

    ui tests

---
+++ app/src/androidTest/java/com/example/social_media_market_place_clone_project/MainActivityTest.java
@@ -0,0 +1,36 @@
+package com.example.social_media_market_place_clone_project;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void doSignUp() {
+        onView(withId(R.id.sign_up_button)).perform(click());
+
+        // Check that the view was changed.
+        onView(withId(R.layout.activity_sign_up)).check(matches(isDisplayed()));
+    }
+
+//    @Test
+//    public void doSignIn() {
+//    }
+//
+//    @Test
+//    public void goToProfile() {
+//    }
+}
