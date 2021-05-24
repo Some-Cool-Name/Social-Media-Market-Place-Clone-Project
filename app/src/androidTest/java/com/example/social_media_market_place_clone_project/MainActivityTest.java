053aa14e5629e46a8d59bcded6c5c191c406cc5f
Vuyo <hlumi68@gmail.com>
2021-05-24 12:29:08 +0200

    mainActivity test

--- app/src/androidTest/java/com/example/social_media_market_place_clone_project/MainActivityTest.java
+++ app/src/androidTest/java/com/example/social_media_market_place_clone_project/MainActivityTest.java
@@ -1,9 +1,13 @@
 package com.example.social_media_market_place_clone_project;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -12,11 +16,12 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class MainActivityTest {
 
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule =
-            new ActivityTestRule<>(MainActivity.class);
+    @Rule public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
     public void doSignUp() {
