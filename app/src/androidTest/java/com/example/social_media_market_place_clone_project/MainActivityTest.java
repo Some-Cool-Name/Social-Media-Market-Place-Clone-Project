50501045f2a01b97cba496c0c42ce923b3e03786
Vuyo <hlumi68@gmail.com>
2021-05-24 13:07:53 +0200

    change manifest

--- app/src/androidTest/java/com/example/social_media_market_place_clone_project/MainActivityTest.java
+++ app/src/androidTest/java/com/example/social_media_market_place_clone_project/MainActivityTest.java
@@ -3,7 +3,6 @@ package com.example.social_media_market_place_clone_project;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
